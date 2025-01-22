f8ff0393831f5dc19185382f0fe5f7fa3a8c4b28
Svetlana Karapetyan <svetlanakarapetyan99@mail.ru>
2025-01-22 14:40:17 +0400

    Controllers

--- src/Scripts/createAdmin.ts
+++ src/Scripts/createAdmin.ts
@@ -9,16 +9,12 @@ async function createAdmin() {
         await AppDataSource.initialize();
         console.log("Data Source has been initialized.");
     }
-
     const userRepository = AppDataSource.getRepository(Users);
-
-    // Check if admin already exists
     const existingAdmin = await userRepository.findOne({ where: { role: UserRole.Admin } });
     if (existingAdmin) {
         console.log("Admin user already exists.");
         return;
     }
-
     const admin = new Users();
     admin.email = "admin@example.com";
     admin.password = await bcrypt.hash("admin_password", 10);
@@ -27,8 +23,6 @@ async function createAdmin() {
     await userRepository.save(admin);
     console.log("Admin user created successfully.");
 }
-
-
 createAdmin().catch((error) => {
     console.error("Error creating admin:", error);
 });
